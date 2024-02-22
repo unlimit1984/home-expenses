4ba9bea17271f0892db13457720b2aec816e5991
unlimit1984 <unlimit_rmc@mail.ru>
2024-02-22 01:33:04 +0100

    [nest] Use ConfigService for expenses service

--- code/frontend/home-expenses-ui/src/app/services/http/expenses-v2.service.ts
+++ code/frontend/home-expenses-ui/src/app/services/http/expenses-v2.service.ts
@@ -2,21 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Expense } from '../../interfaces/Expense';
-import { environment } from '../../../environments/environment';
+import { ConfigService } from '../config/config.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExpensesV2Service {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient, private configService: ConfigService) {
+    this.apiUrl = this.configService.config.API;
+  }
 
-  private url = environment.authApiUrl;
+  private readonly apiUrl: string;
 
   public getExpenses(): Observable<Expense[]> {
-    return this.httpClient.get<Expense[]>(`${this.url}/api/expense/findAll`);
+    return this.httpClient.get<Expense[]>(`${this.apiUrl}/api/expense/findAll`);
   }
 
   public createExpense(expenseBody: Expense): Observable<Expense> {
-    return this.httpClient.post<Expense>(`${this.url}/api/expense/create`, expenseBody);
+    return this.httpClient.post<Expense>(`${this.apiUrl}/api/expense/create`, expenseBody);
   }
 }
