d30658c99779301a71d2be61420dd90c54bf62f6
unlimit1984 <unlimit_rmc@mail.ru>
2024-03-12 22:57:59 +0100

    [devops] Add workflow for displaying secrets
    - Fix #2

--- decode_secret.js
+++ decode_secret.js
@@ -1,7 +1,8 @@
-// const { Buffer } = require("buffer");
-//
-// const encodedSecret = process.env.UI_URL; // Access the encoded secret from environment variables
-// const decodedSecret = Buffer.from(encodedSecret, "base64").toString("utf-8");
-// console.log(decodedSecret);
-
 console.log('Hello from my Node.js script!');
+
+const { Buffer } = require("buffer");
+
+const encodedSecret = process.env.UI_URL; // Access the encoded secret from environment variables
+console.log('encodedSecret', encodedSecret);
+const decodedSecret = Buffer.from(encodedSecret, "base64").toString("utf-8");
+console.log(decodedSecret);
