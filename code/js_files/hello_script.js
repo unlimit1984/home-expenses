837dae3832e98db63a5176ee9f41305d9fa5705c
unlimit1984 <unlimit_rmc@mail.ru>
2024-03-17 14:23:37 +0100

    [devops] Add scheduled rotate tokens workflow
    - Fix #46

--- code/js_files/hello_script.js
+++ code/js_files/hello_script.js
@@ -1,1 +1,6 @@
-console.log('Hello from js script')
+console.log("Hello from js script");
+console.log("process.argv", process.argv); // Print all command-line arguments
+console.log("process.argv[2]", process.argv[2]); // Print the first argument after the JavaScript file path
+console.log("process.argv[3]", process.argv[3]); // Print the second argument after the JavaScript file path
+console.log("process.argv[4]", process.argv[4]); // Print the third argument after the JavaScript file path
+console.log("process.argv[5]", process.argv[5]); // Print the forth argument after the JavaScript file path
