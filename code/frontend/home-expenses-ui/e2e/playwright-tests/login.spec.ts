70dd2a71a213d6415b383db0bf9ba7a77d8cabfd
unlimit1984 <unlimit_rmc@mail.ru>
2024-04-20 22:10:16 +0200

    [F,e2e][57444579] Add login test
    - Uncomment all tests
    - Use `ubuntu-latest`

--- code/frontend/home-expenses-ui/e2e/playwright-tests/login.spec.ts
+++ code/frontend/home-expenses-ui/e2e/playwright-tests/login.spec.ts
@@ -8,69 +8,64 @@ const { chromium } = require('playwright');
 test.describe('login', () => {
   let username = process.env.PLAYWRIGHT_USERNAME;
   let password = process.env.PLAYWRIGHT_PASSWORD;
-  let appUrl = process.env.APP_URL;
+  let appUrl = process.env.APP_URL ? process.env.APP_URL : 'https://local.home-expenses.com:8444';
 
   test.beforeAll(async () => {
     await chromium.launch({
       ignoreHTTPSErrors: true
     });
   });
-  test.beforeEach(() => {});
 
-  test.skip('has correct title', async ({ page }) => {
-    await page.goto('https://local.home-expenses.com:8444');
+  test('has correct title', async ({ page }) => {
+    await page.goto(appUrl);
     await expect(page).toHaveTitle(/Home Expenses/);
   });
 
-  test.skip('redirected to login page', async ({ page }) => {
-    await page.goto('https://local.home-expenses.com:8444');
+  test('redirected to login page', async ({ page }) => {
+    await page.goto(appUrl);
 
     await page.waitForURL('**/auth/signin');
-    await expect(page.url()).toBe('https://local.home-expenses.com:8444/auth/signin');
+    expect(page.url()).toBe(`${appUrl}/auth/signin`);
     await page.waitForSelector('input[id="loginName"]');
     await page.waitForSelector('input[id="loginPassword"]');
   });
 
-  test.skip('redirected to Unauthorized page if login failed', async ({ page }) => {
-    await page.goto('https://local.home-expenses.com:8444');
+  test('redirected to Unauthorized page if login failed', async ({ page }) => {
+    await page.goto(appUrl);
 
     await page.waitForURL('**/auth/signin');
-    await expect(page.url()).toBe('https://local.home-expenses.com:8444/auth/signin');
+    expect(page.url()).toBe(`${appUrl}/auth/signin`);
     await page.waitForSelector('input[id="loginName"]');
     await page.waitForSelector('input[id="loginPassword"]');
 
-    await page.getByRole('textbox', { name: 'email' }).fill('');
+    await page.getByRole('textbox', { name: 'email' }).fill('bad-user@mail.com');
     await page.getByRole('textbox', { name: 'password' }).fill('bad-password');
     await page.waitForSelector('button[data-test-id="loginBtn"]');
     await page.click('button[data-test-id="loginBtn"]');
 
-    await page.waitForURL('**/all-expenses1');
-    await expect(page.url()).toBe('https://local.home-expenses.com:8444/all-expenses');
+    await page.waitForURL('**/401');
+    expect(page.url()).toBe(`${appUrl}/401`);
   });
 
-  test.skip('redirected to `/all-expenses` after successful login', async ({ page }) => {
-    await page.goto('https://local.home-expenses.com:8444');
+  test('redirected to `/all-expenses` after successful login', async ({ page }) => {
+    await page.goto(appUrl);
 
     await page.waitForURL('**/auth/signin');
-    await expect(page.url()).toBe('https://local.home-expenses.com:8444/auth/signin');
+    expect(page.url()).toBe(`${appUrl}/auth/signin`);
     await page.waitForSelector('input[id="loginName"]');
     await page.waitForSelector('input[id="loginPassword"]');
 
-    await page.getByRole('textbox', { name: 'email' }).fill('');
-    await page.getByRole('textbox', { name: 'password' }).fill('');
+    await page.getByRole('textbox', { name: 'email' }).fill(username);
+    await page.getByRole('textbox', { name: 'password' }).fill(password);
     await page.waitForSelector('button[data-test-id="loginBtn"]');
     await page.click('button[data-test-id="loginBtn"]');
 
-    await page.waitForURL('**/all-expenses1');
-    await expect(page.url()).toBe('https://local.home-expenses.com:8444/all-expenses');
+    await page.waitForURL('**/all-expenses');
+    expect(page.url()).toBe(`${appUrl}/all-expenses`);
   });
 
   test('Go to Settings', async ({ page }) => {
-    if (!appUrl) {
-      appUrl = 'https://local.home-expenses.com:8444';
-    }
     await page.goto(appUrl);
-    // await page.goto('https://local.home-expenses.com:8444');
 
     await page.waitForURL('**/auth/signin');
     expect(page.url()).toBe(`${appUrl}/auth/signin`);
@@ -88,11 +83,5 @@ test.describe('login', () => {
     await page.waitForSelector('a[data-test-id="settings-link-desktop"]');
     await page.click('a[data-test-id="settings-link-desktop"]');
     await page.waitForURL('**/settings');
-  });
-
-  test.skip('Print env var', async ({ page }) => {
-    console.log('Print env var');
-    console.log('process.env.PLAYWRIGHT_USERNAME', process.env.PLAYWRIGHT_USERNAME);
-    console.log('process.env.PLAYWRIGHT_PASSWORD', process.env.PLAYWRIGHT_PASSWORD);
   });
 });
