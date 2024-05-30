7d0990ec22b2d9f7f1f2f365322108efc43f45b2
unlimit1984 <unlimit_rmc@mail.ru>
2024-05-30 22:57:10 +0200

    [F][57271605] Add ChartJs
    - Add some initial BarChart

--- code/frontend/home-expenses-ui/src/app/pages/expenses/expenses.component.ts
+++ code/frontend/home-expenses-ui/src/app/pages/expenses/expenses.component.ts
@@ -13,6 +13,8 @@ import { getExpenses } from '../../store/expenses/expenses.actions';
 import { Observable, Subject, takeUntil } from 'rxjs';
 import { selectExpenses, selectIsLoading } from '../../store/expenses/expenses.selectors';
 import { formatDate } from '@angular/common';
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartConfiguration, ChartData, ChartEvent } from 'chart.js';
 
 export interface Transaction {
   date: string;
@@ -81,5 +83,59 @@ export class ExpensesComponent implements OnInit, OnDestroy {
 
   public getFormattedDate(timestamp: string): string {
     return formatDate(new Date(Number(timestamp)), 'YYYY-MM-dd HH:mm:ss', this.locale);
+  }
+
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective<'bar'> | undefined;
+
+  public barChartOptions: ChartConfiguration<'bar'>['options'] = {
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: {
+      x: {},
+      y: {
+        min: 10
+      }
+    },
+    plugins: {
+      legend: {
+        display: true
+      }
+      // datalabels: {
+      //   anchor: 'end',
+      //   align: 'end'
+      // }
+    }
+  };
+  public barChartType = 'bar' as const;
+
+  public barChartData: ChartData<'bar'> = {
+    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+    datasets: [
+      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+    ]
+  };
+
+  // events
+  public chartClicked({ event, active }: { event?: ChartEvent; active?: object[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event?: ChartEvent; active?: object[] }): void {
+    console.log(event, active);
+  }
+
+  public randomize(): void {
+    // Only Change 3 values
+    this.barChartData.datasets[0].data = [
+      Math.round(Math.random() * 100),
+      59,
+      80,
+      Math.round(Math.random() * 100),
+      56,
+      Math.round(Math.random() * 100),
+      40
+    ];
+
+    this.chart?.update();
   }
 }
