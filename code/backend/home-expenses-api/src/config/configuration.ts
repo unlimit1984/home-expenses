53ca6e71bd68497e38d559ade87e93c936aa0043
unlimit1984 <unlimit_rmc@mail.ru>
2025-08-05 01:26:06 +0200

    [F] Fix backend errors - See #79
    - Test Fix 5

--- code/backend/home-expenses-api/src/config/configuration.ts
+++ code/backend/home-expenses-api/src/config/configuration.ts
@@ -3,24 +3,29 @@
  */
 
 import * as process from 'process';
+import { Logger } from '@nestjs/common';
 
-export default () => ({
-  port: parseInt(process.env.PORT, 10) || 3000,
-  uiUrl: process.env.UI_URL,
-  database: {
-    port: process.env.DB_PORT,
-    host: process.env.DB_HOST,
-    name: process.env.DB_NAME,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD
-  },
-  mail: {
-    host: process.env.MAIL_SMTP_SERVICE,
-    username: process.env.MAIL_USERNAME,
-    appPassword: process.env.MAIL_APP_PASSWORD
-  },
-  auth: {
-    at_secret: process.env.AUTH_AT_SECRET,
-    rt_secret: process.env.AUTH_RT_SECRET
-  }
-});
+export default () => {
+  const config = {
+    port: parseInt(process.env.PORT, 10) || 3000,
+    uiUrl: process.env.UI_URL,
+    database: {
+      port: process.env.DB_PORT,
+      host: process.env.DB_HOST,
+      name: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD
+    },
+    mail: {
+      host: process.env.MAIL_SMTP_SERVICE,
+      username: process.env.MAIL_USERNAME,
+      appPassword: process.env.MAIL_APP_PASSWORD
+    },
+    auth: {
+      at_secret: process.env.AUTH_AT_SECRET,
+      rt_secret: process.env.AUTH_RT_SECRET
+    }
+  };
+  Logger.debug('config=' + JSON.stringify(config));
+  return config;
+};
